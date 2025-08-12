0696104302c80b8e2d0343dc6f9df9132c88d43f
usmangujjar645 <zaryangujjar512@gmail.com>
2025-08-12 02:30:05 -0700

    first commit

--- python/function.py
+++ python/function.py
@@ -18,4 +18,26 @@ b = 200
 result = add_fun(a,b)
 print(result)
 
-    
+# def profil( a,a)
+
+# dict = {
+#      'a' :5,
+#      'age': 20,
+#      'city': "lahour",
+#      'email': "osman@gmail.com", 
+def  math_fun():
+    a = a+b
+    b = a-b
+    c = a*c
+    d = a/d 
+    print("return value")
+    return a,b,c,d,
+a =10
+b = 20 
+c =15
+d =30
+
+
+
+
+   
