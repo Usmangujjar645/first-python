1d10f95d8510c328a2e6213ecb455ffb62f6c0c3
usmangujjar645 <zaryangujjar512@gmail.com>
2025-08-11 20:53:12 -0700

    read: if condition

---
+++ python/first.py
@@ -0,0 +1,38 @@
+name = "usman"
+print(name)
+
+
+a = 10
+b = 10
+print(a+b)
+ # Celsius temperature store karein
+celsius = 25  
+
+# Celsius to Fahrenheit conversion
+fahrenheit = (celsius * 9/5) + 32  
+
+# Result print karein
+print("Temperature in Fahrenheit:", fahrenheit)
+# Ek list banao jisme tumhare 5 favourite fruits hon aur usko print karo
+list = [" mango,bnana ,apple"]
+print(list)
+
+#  lenth of list
+list1 = [" mango,bnana ,apple"]
+print(len(list1))
+# list add
+list2 = [" mango,bnana ,apple"]
+list2.append("peach")
+print("mango,bnana ,apple:",list2)
+
+
+# Example list elemint
+fruits = ["Apple", "Banana", "Mango", "Orange"]
+
+# Second element print karein
+print(fruits[1])
+
+
+animal =["lin, pegion,chiken,"]
+animal.pop("lin")
+print(animal)
