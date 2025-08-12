5d5de28470c0bc9fbba7b546e85757bafcc04973
usmangujjar645 <zaryangujjar512@gmail.com>
2025-08-11 21:41:52 -0700

    function.py

---
+++ python/function.py
@@ -0,0 +1,21 @@
+def git_func():
+    a = 5
+    b = 10 
+    c = a + b
+    return c
+result = git_func()
+print("the result",result)
+
+
+
+def add_fun(a ,b):
+    c =a + b
+    print( "return value")
+    return c
+a = 100
+b = 200
+ 
+result = add_fun(a,b)
+print(result)
+
+    
